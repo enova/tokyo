31fc81a9e6354e682db604b55591a8d4a0b929ac
Zamir Syed <zsyed@enova.com>
2016-01-12 22:01:22 -0600

    Removed durations array. Click() now only calls time.Now() once.

--- src/stopwatch/stopwatch_test.go
+++ src/stopwatch/stopwatch_test.go
@@ -6,34 +6,39 @@ import (
 )
 
 func TestOnlyOneClickBeforeShow(t *testing.T) {
-	s := New()
+	assert := assert.New(t)
 
+	s := New()
 	s.Click()
-	label, e := s.Show("Hello")
-	assert.Error(t, e, "Expected to error because there has only been 1 click")
-	assert.Equal(t, label, "", "Expected no label because there has only been 1 click")
+
+	label, err := s.Show("Hello")
+	assert.Error(err, "Expected to error because there has only been 1 click")
+	assert.Equal(label, "", "Expected no label because there has only been 1 click")
 }
 
 func TestNormalShow(t *testing.T) {
-	s := New()
+	assert := assert.New(t)
 
+	s := New()
 	s.Click()
 	s.Click()
-	label, e := s.Show("Hello")
-	assert.NoError(t, e, "Expected no error because there has been 2 clicks")
-	assert.Contains(t, label, "Hello", "Expected label because there has been 2 clicks")
+
+	label, err := s.Show("Hello")
+	assert.NoError(err, "Expected no error because there has been 2 clicks")
+	assert.Contains(label, "Hello", "Expected label because there has been 2 clicks")
 }
 
 func TestMultipleShows(t *testing.T) {
-	s := New()
+	assert := assert.New(t)
 
+	s := New()
 	s.Click()
 	s.Click()
 
 	label, _ := s.Show("Hello")
-	assert.Contains(t, label, "Hello", "Expected label because there has been 2 clicks")
+	assert.Contains(label, "Hello", "Expected label because there has been 2 clicks")
 
 	s.Click()
 	newLabel, _ := s.Show("Hello")
-	assert.NotEqual(t, label, newLabel, "Expected new label because there has been 3 clicks")
+	assert.NotEqual(label, newLabel, "Expected new label because there has been 3 clicks")
 }
